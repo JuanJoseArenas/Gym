56754d580e854e295edbdf8ba2bde9e13be45981
Juan Jose Arenas <juanjoseaq99@gmail.com>
2024-11-28 12:17:54 -0500

    Creacion del proyecto para registar un plan de un gimnasio

---
+++ src/main/java/co/edu/uco/init/RegistrarPlanApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrarPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrarPlanApplication.class, args);
+	}
+
+}
